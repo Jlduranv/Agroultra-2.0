eb305ce6965ae1f2e58bce71dfe2d3817339fabd
joseluisduran <joseluisduran@JOSELUISDURAE0E>
2020-10-19 22:22:10 -0300

    Agregar archivos de proyecto.

---
+++ EVALUACION_2_TP/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace EVALUACION_2_TP {
+    public class BundleConfig {
+        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles) {           
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                    "~/url/https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css",
+                    "~/Content/dist/magnific-popup.css",
+                    "~/Content/css/styles.css"));
+        }
+    }
+}
---
+++ EVALUACION_2_TP/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EVALUACION_2_TP.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+        
+        public ActionResult Registro_Fruto()
+        {
+            return View();
+        }
+        public ActionResult Registro_Sectores()
+        {
+            return View();
+        }
+        public ActionResult Menu_Opciones()
+        {
+            return View();
+        }
+        public ActionResult Registro_Usuario()
+        {
+            return View();
+        }
+        public ActionResult Ingreso()
+        {
+            return View();
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
