88b95eff9721806d0e9e725ef763ebf0d9d40253
EmilioOlivares <made.in.lord@gmail.com>
2020-11-05 00:23:48 -0300

    arreglos para conectar a bdd

--- EVALUACION_2_TP/Controllers/bddController.cs
+++ EVALUACION_2_TP/Controllers/bddController.cs
@@ -20,23 +20,23 @@ namespace EVALUACION_2_TP.Controllers
 
         public ActionResult mostrar()
         {
-            SqlConnection con = new SqlConnection(data);
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=bdd;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             var sentencia = new SqlCommand();
             SqlDataReader dr;
             sentencia.Connection = con;
-            sentencia.CommandText = "select * from contactos";
+            sentencia.CommandText = "select * from usuarios";
             sentencia.CommandType = System.Data.CommandType.Text;
             con.Open();
             dr = sentencia.ExecuteReader();
             var mensaje = "";
             while(dr.Read())
             {
-                mensaje = mensaje + dr["rut"].ToString();
+                mensaje = mensaje + dr["nombre"].ToString();
 
             }
             ViewBag.mensaje = mensaje;
             con.Close();
-            return View("/Views/bdd/respuestas.cshtml");
+            return View("/Views/bdd/respuesta.cshtml");
         }
     }
 }
